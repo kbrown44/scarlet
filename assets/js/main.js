bdca89bf6846447eb96156e8c960104b243bdcd7
kbrown44 <kbswr444@gmail.com>
2017-07-05 11:12:10 -0400

    Image changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,38 +8,41 @@ var x = 0;
 
 //******MENU APPEAR******
 
-logoShift.onclick = function () {
-  if (x == 0) {   
-      menuAppear.style.display = "block";
-      logoShift.style.margin = "2em 0em 0.5em 0";
-      x = 1;
-    }
+  logoShift.onclick = function () {
+    if (x == 0) {   
+        menuAppear.style.display = "block";
+        logoShift.style.margin = "2em 0 0.5em 0";
+        x = 1;
+      }
   
-  else {   
-      menuAppear.style.display = "none";
-      logoShift.style.margin = "2em 0em 3em 0";
-      x = 0;
-    }
-}
-
+    else {   
+        menuAppear.style.display = "none";
+        logoShift.style.margin = "2em 0 3em 0";
+        x = 0;
+      }
+  }
+  
+  function menuClose() {
+    menuAppear.style.display = "none";
+    logoShift.style.margin = "2em 0 3em 0";
+    x = 0;
+    
+  }
 
 
 
 //******ANIMATIONS******
 
 logoShift.onmouseout = function () {
-  logoShift.style.animation = "spin4 0.3s";
+  logoShift.style.animation = "spin4 0.2s";
   logoShift.style.animationFillMode = "forwards";
   logoShift.style.animationTimingFunction = "ease";
   logoShift.style.animationIterationCount = "1";
 }
 
 logoShift.onmouseover = function () {
-  logoShift.style.animation = "spin3 0.5s";
+  logoShift.style.animation = "spin3 0.4s";
   logoShift.style.animationFillMode = "forwards";
   logoShift.style.animationTimingFunction = "ease";
   logoShift.style.animationIterationCount = "1";
 }
-
-
-
