611dd3ad3f983ec98c5fdea06ff274cfd760185a
kbrown44 <kbswr444@gmail.com>
2017-07-13 14:31:24 -0400

    Move Scripts To JS File

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,10 @@
 
 var logoShift = document.getElementById("logo");
 var menuAppear = document.getElementById("menuTexts");
+var left = document.getElementById("left");
+var right = document.getElementById("right");
+var gallery = document.getElementById("gallery");
 var x = true;
-
 
 
 //******MENU APPEAR******
@@ -46,3 +48,47 @@ logoShift.onmouseover = function () {
   logoShift.style.animationTimingFunction = "ease";
   logoShift.style.animationIterationCount = "1";
 }
+
+$(document).ready(function(){
+        var dir = "images/thumbnail/"; // folder location
+        var fileextension = ".jpg"; // image format
+        var i = "1";
+        
+        $(function imageloop(){
+          $("<div  />").attr('class', "picDiv" ).attr('style', "background-image:url(" + dir + i + fileextension + ")" ).appendTo("#gallery");
+          //$("<img />").attr('src', dir + i + fileextension ).appendTo("#testDiv" + i);
+          if (i<15){
+            i++;
+            imageloop();
+          };
+        });   
+      });
+
+
+//******SCROLL GALLERY******
+
+var amount = '';
+
+function scroll() {
+  $('#gallery').animate({
+    scrollLeft: amount
+  }, 100, 'linear',function() {
+      if (amount != '') {
+        scroll();
+      }
+    });
+}
+
+$('#right').hover(function() {
+  amount = '+=20';
+  scroll();
+}, function() {
+      amount = '';
+    });
+
+$('#left').hover(function() {
+   amount = '-=20';
+   scroll();
+}, function() {
+      amount = '';
+    });
